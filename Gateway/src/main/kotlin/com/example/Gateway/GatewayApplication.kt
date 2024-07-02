8d1bb3b3f74e154ee84b1a36bec9241b3e8e25f1
Kevin-av <116099021+Kevin-av@users.noreply.github.com>
2024-07-01 22:30:18 -0500

    Nuevos cambios

--- Gateway/src/main/kotlin/com/example/Gateway/GatewayApplication.kt
+++ Gateway/src/main/kotlin/com/example/Gateway/GatewayApplication.kt
@@ -2,6 +2,7 @@ package com.example.Gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
 class GatewayApplication
